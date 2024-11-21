e08f66ad71d49f339ed9653fa2b824d0e6f13402
Arceus <109497923+connect-arceus@users.noreply.github.com>
2024-11-21 13:44:31 -0800

    env file added

--- apps/web/src/firestore/store.ts
+++ apps/web/src/firestore/store.ts
@@ -2,15 +2,16 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import "dotenv/config"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjwCI5jg2k4pl-0f9fAdhm-dBGT6hk5Lg",
-  authDomain: "chess-game-aca46.firebaseapp.com",
-  projectId: "chess-game-aca46",
-  storageBucket: "chess-game-aca46.appspot.com",
-  messagingSenderId: "426846498870",
-  appId: "1:426846498870:web:299cdc1e5157fbabe92897"
+  apiKey: process.env.VITE_FIRESTORE_API_KEY,
+  authDomain: process.env.VITE_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIRESTORE_MESSENGER_ID  ,
+  appId: process.env.VITE_FIRESTORE_APP_ID
 };
 
 // Initialize Firebase
